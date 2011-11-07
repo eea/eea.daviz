757cc44f610466165e18732d9628f3c76fb669ec
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-11-07 22:07:19 +0000

    Added documentation. Updated HISTORY.txt to fit Plone style.

--- setup.py
+++ setup.py
@@ -4,17 +4,15 @@ import os
 from os.path import join
 from setuptools import setup, find_packages
 
-name = 'eea.daviz'
-path = name.split('.') + ['version.txt']
-version = open(join(*path)).read().strip()
+NAME = 'eea.daviz'
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(join(*PATH)).read().strip()
 
-setup(name=name,
-      version=version,
+setup(name=NAME,
+      version=VERSION,
       description="EEA DaViz",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",

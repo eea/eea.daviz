bdb96933ca0b5051c015fe6b8da4f5a3b9354778
szabozo0 <szabozo0@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-08-31 14:33:42 +0000

    Refs #5449; Cleanup eea.daviz as configuration will be implemented in eea.app.visualization

--- eea/daviz/interfaces.py
+++ eea/daviz/interfaces.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 """ Daviz interfaces module
 """
-
-from zope import schema
-from zope.interface import Interface
-
 # BBB Subtypes
 from eea.app.visualization.interfaces import \
      IPossibleVisualization as IPossibleExhibitJson
@@ -20,13 +16,3 @@ __all__ = [
     IDavizRelationsChangedEvent.__name__,
 ]
 
-
-class IDavizSettings(Interface):
-    """Daviz Settings
-    """
-    defaultFolder = schema.Choice(
-        title=u"Default Folder",
-        description=u"Default Folder for creating Visualizations",
-        required=True,
-        vocabulary='eea.daviz.vocabularies.DefaultFolderVocabulary',
-        )

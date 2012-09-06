b5315301607681a422104e4bd46b3370332ee2d4
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-09-06 07:30:53 +0000

    Refs #5428 - eea.daviz   - Updated Plone4 demo buildout.

--- buildouts/plone4/install.sh
+++ buildouts/plone4/install.sh
@@ -1,23 +1,7 @@
 #!/bin/sh
-
-if [ -s "bin/activate" ]; then
-  echo "Buildout is already installed. "
-  echo "Please remove bin/activate if you want to re-run this script."
-  exit 0
+if [ ! -f bootstrap.sh ]
+then
+    wget -O bootstrap.sh https://svn.eionet.europa.eu/repositories/Zope/trunk/www.eea.europa.eu/trunk/install.sh
 fi
-
-ZCBUILDOUT=`grep "zc\.buildout" versions.cfg | sed 's/=/==/g'`
-
-echo "Installing virtualenv"
-wget "http://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.5.2.tar.gz" -O "/tmp/virtualenv-1.5.2.tar.gz"
-tar -zxvf "/tmp/virtualenv-1.5.2.tar.gz" -C "/tmp/"
-
-echo "Running: python2.6 virtualenv.py --clear --no-site-packages ."
-python2.6 "/tmp/virtualenv-1.5.2/virtualenv.py" --clear --no-site-packages .
-rm "/tmp/virtualenv-1.5.2.tar.gz"
-rm -r "/tmp/virtualenv-1.5.2"
-
-echo "Installing zc.buildout: $ ./bin/easy_install" $ZCBUILDOUT
-./bin/easy_install $ZCBUILDOUT
-
-echo "All set. Now you can run ./bin/buildout"
+chmod u+x bootstrap.sh
+./bootstrap.sh

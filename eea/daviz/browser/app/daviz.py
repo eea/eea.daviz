d507f0bc2c9c0bcc721798dc5d139e0372258eac
Petchesi-Iulian <iamnotacat12@gmail.com>
2019-12-03 11:40:49 +0100

    Refs #110463 Added a daviz custom adapter for image scales based on plone.scale annotation storage.

--- eea/daviz/browser/app/daviz.py
+++ eea/daviz/browser/app/daviz.py
@@ -3,7 +3,14 @@
 from zope.component.hooks import getSite
 from zope.component import queryUtility
 from zope.container.interfaces import INameChooser
+from zope.interface import implementer
 from eea.app.visualization.controlpanel.interfaces import IDavizSettings
+from eea.depiction.interfaces import IRecreateScales
+from plone.scale.storage import AnnotationStorage
+from plone.scale.scale import scaleImage
+from uuid import uuid4
+from time import time
+
 
 class Daviz(object):
     """ Daviz
@@ -42,3 +49,56 @@ class Daviz(object):
         newObj.title = self.context.title
         newObj.setRelatedItems([self.context])
         self.request.response.redirect(newObj.absolute_url()+"/daviz-edit.html")
+
+
+@implementer(IRecreateScales)
+class RecreateScales(object):
+    """ Recreate image scales daviz custom adapter
+    """
+    def __init__(self, context):
+        self.context = context
+
+    def __call__(self, fieldname='image'):
+        image_view = self.context.restrictedTraverse('@@imgview')
+        image = getattr(image_view, 'img', None)
+        if not image:
+            raise AttributeError(fieldname)
+        if getattr(image, 'portal_type', '') == 'Image':
+            field = image.getField(fieldname)
+            if not field:
+                raise AttributeError(fieldname)
+            field.removeScales(image)
+            field.createScales(image)
+        else:
+            # use plone.scale annotation storage
+            image_data = image()
+            scale_result = scaleImage(image_data, width=image.width(), height=image.height())
+            storage = AnnotationStorage(self.context, time)
+
+            if scale_result is not None:
+                # storage will be modified:
+                # good time to also cleanup
+                parameters = {
+                    'image_hash': image.__hash__(),
+                    'modified': time()
+                }
+
+                key = storage.hash(**parameters)
+                storage._cleanup()
+                data, format_, dimensions = scale_result
+                width, height = dimensions
+                uid = str(uuid4())
+                info = dict(
+                    uid=uid,
+                    data=data,
+                    width=width,
+                    height=height,
+                    mimetype='image/{0}'.format(format_.lower()),
+                    key=key,
+                    modified=storage.modified_time,
+                )
+                try:
+                    storage.storage[uid] = info
+                except:
+                    import pdb; pdb.set_trace()
+        return info or None

2c3dc0ecadd3890915a344719fdfc7cffa539f85
Petchesi-Iulian <iamnotacat12@gmail.com>
2019-12-04 13:58:46 +0100

    Refs #110463 use plone.app.blob store scale for scales.

--- eea/daviz/browser/app/daviz.py
+++ eea/daviz/browser/app/daviz.py
@@ -1,15 +1,16 @@
 """ Create visualizations with datasources
 """
+from Acquisition import aq_base
+# from Products.Archetypes.Storage.annotation import AnnotationStorage
+from ZODB.blob import Blob
+from eea.app.visualization.controlpanel.interfaces import IDavizSettings
+from eea.depiction.interfaces import IRecreateScales
+from plone.app.blob.config import blobScalesAttr
+from plone.scale.scale import scaleImage
 from zope.component.hooks import getSite
 from zope.component import queryUtility
 from zope.container.interfaces import INameChooser
 from zope.interface import implementer
-from eea.app.visualization.controlpanel.interfaces import IDavizSettings
-from eea.depiction.interfaces import IRecreateScales
-from plone.scale.storage import AnnotationStorage
-from plone.scale.scale import scaleImage
-from uuid import uuid4
-from time import time
 
 
 class Daviz(object):
@@ -63,42 +64,41 @@ class RecreateScales(object):
         image = getattr(image_view, 'img', None)
         if not image:
             raise AttributeError(fieldname)
+
+        info = None
         if getattr(image, 'portal_type', '') == 'Image':
             field = image.getField(fieldname)
             if not field:
                 raise AttributeError(fieldname)
+            # import pdb; pdb.set_trace()
             field.removeScales(image)
             field.createScales(image)
         else:
-            # use plone.scale annotation storage
+            # use plone.app.blob store scale
+            # storage = AnnotationStorage(self.context)
             image_data = image()
-            scale_result = scaleImage(image_data, width=image.width(), height=image.height())
-            storage = AnnotationStorage(self.context, time)
 
-            if scale_result is not None:
-                # storage will be modified:
-                # good time to also cleanup
-                parameters = {
-                    'image_hash': image.__hash__(),
-                    'modified': time()
-                }
-
-                key = storage.hash(**parameters)
-                storage._cleanup()
-                data, format_, dimensions = scale_result
-                width, height = dimensions
-                uid = str(uuid4())
-                info = dict(
-                    uid=uid,
-                    data=data,
-                    width=width,
-                    height=height,
-                    mimetype='image/{0}'.format(format_.lower()),
-                    key=key,
-                    modified=storage.modified_time,
-                )
-                try:
-                    storage.storage[uid] = info
-                except:
-                    import pdb; pdb.set_trace()
-        return info or None
+            for name, size in image.sizes.items():
+                width, height = size
+                scale_result = scaleImage(image_data, width=width, height=height)
+                if scale_result is not None:
+                    id = fieldname + "_" + name
+                    data, format_, dimensions = scale_result
+                    info = dict(
+                        data=data,
+                        id = id,
+                        content_type='image/{0}'.format(format_.lower()),
+                        filename='',
+                    )
+                    # self.getStorage(instance).unset(id, instance, **kwargs)
+                    # import pdb; pdb.set_trace()
+                    fields = getattr(aq_base(self.context), blobScalesAttr, {})
+                    scales = fields.setdefault(fieldname, {})
+                    info['blob'] = Blob()
+                    blob = info['blob'].open('w')
+                    blob.write(info['data'])
+                    blob.close()
+                    del info['data']
+                    scales[name] = info
+                    setattr(self.context, blobScalesAttr, fields)
+        return info
